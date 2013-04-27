66cf09d6a4ebd212f65705fd42909d12847a1e80
Daniel Erickson <dan@Macintosh-2.local>
2013-04-26 19:40:17 -0700

    initial commit (heroku template)

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var path = require("path");
+var app = express();
+app.use(express.logger());
+app.use(express.static(path.join(__dirname, 'site')));
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
