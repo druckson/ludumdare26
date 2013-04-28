cb2e54129d1dd38354ff8f8fb97a404e503ba194
Daniel Erickson <dan@Macintosh-2.local>
2013-04-28 15:32:31 -0700

    add caching

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var path = require("path");
 var app = express();
 app.use(express.logger());
-app.use(express.static(path.join(__dirname, 'site')));
+app.use(express.static(path.join(__dirname, 'site'), {maxAge: 300000}));
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
